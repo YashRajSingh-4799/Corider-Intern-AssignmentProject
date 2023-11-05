c17c688fb3ace6862f742e4f7651c4e2c108b2e2
Yash Raj Singh <yashrajsingh4799@gmail.com>
2023-11-05 23:27:13 +0530

    basic structure and UI checkpoint

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,106 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import React from 'react';
+// import logo from './logo.svg';
+import { Grid, GridItem } from "@chakra-ui/react";
+import ChatMessage from "./components/Chat";
+import ChatInput from "./components/Input";
+import ChatHeader from "./components/Header";
+// import database from "./components/database";
+
+import React, { useState, useRef, useEffect } from "react";
 
 function App() {
+  const [chatMessages, setChatMessages] = useState<any[]>([]);
+  const [onlineStatus, setOnlineStatus] = useState<string>("Online");
+  const [pageNumber, setPageNumber] = useState<number>(0);
+  const [chatsLoaded, setChatsLoaded] = useState<boolean>(false);
+  const chatContainerRef = useRef<HTMLDivElement | null>(null);
+  let currentDate: string = "";
+  let newDate: string = "";
+
+ 
+  useEffect(() => {
+    const container = chatContainerRef.current;
+    if (container) {
+      container.addEventListener("scroll", handleScroll);
+    }
+    return () => {
+      if (container) {
+        container.removeEventListener("scroll", handleScroll);
+      }
+    };
+  }, []);
+
+  useEffect(() => {
+    fetchChatData();
+    setTimeout(() => {
+      setChatsLoaded(true);
+    }, 2000);
+  }, [pageNumber]);
+
+  useEffect(() => {
+    if (chatContainerRef.current) {
+      chatContainerRef.current.scrollTop = 200;
+    }
+  }, [pageNumber]);
+
+  useEffect(() => {
+    if (chatsLoaded && chatContainerRef.current) {
+      chatContainerRef.current.scrollTop =
+        chatContainerRef.current.scrollHeight;
+    }
+  }, [chatsLoaded]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="ChatApp">
+      <Grid templateRows="2fr 8fr 1.5fr" height="100vh" gap="10px">
+        <GridItem>
+          <ChatHeader />
+          {onlineStatus === "Offline" && (
+            <p
+              style={{
+                color: "red",
+                textAlign: "center",
+                backgroundColor: "#ffd400",
+                padding: "6px",
+                margin: "-10px -20px",
+              }}
+            >
+              You are offline!! Go online to view new chats.
+            </p>
+          )}
+        </GridItem>
+        <GridItem className="chatContainer" ref={chatContainerRef}>
+          <div className="chatDiv">
+            {chatMessages
+              .slice()
+              .reverse()
+              .map((message, index) => {
+                let dateChanged = false;
+                if (index === 0) {
+                  newDate = message.time.slice(0, 10);
+                  currentDate = newDate;
+                }
+                currentDate = newDate;
+                if (currentDate !== message.time.slice(0, 10)) {
+                  dateChanged = true;
+                  newDate = message.time.slice(0, 10);
+                }
+
+                return (
+                  <ChatMessage/>
+                );
+              })}
+          </div>
+        </GridItem>
+        <GridItem>
+          <ChatInput />
+        </GridItem>
+      </Grid>
     </div>
   );
 }
+
+
+
 
 export default App;
